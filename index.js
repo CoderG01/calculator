ae8f2c4f913cdbe4d21c2eaa5e3e26847729cfb5
Coder_G <harshgoswami1105@gmail.com>
2022-10-03 14:43:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+function myfun() {
+    document.getElementById("form").style.top = "250px"
+    document.getElementById("home_btn").style.display = "none";
+}
+
+function my_close() {
+    document.getElementById("form").style.top = "-700px"
+    document.getElementById("home_btn").style.display = "block";
+}
